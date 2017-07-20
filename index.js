a47c61f9e5a306a032a6c386868385374ed1ce30
Tom Milligan <tom@tommilligan.net>
2017-07-20 22:46:56 +0100

    Working normalisation of BBC news articles

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
-const ner = require('ner');
+var axios = require('axios');
+var Promise = require("bluebird");
 
-var body = `John Smith walked to the McDonalds in Antarctica to buy dinner.`
-if (process.argv[2]) {
-	body = process.argv[2]
-}
+var ner = require('ner');
+var cheerio = require('cheerio')
 
-normalize = (rawText) => {
-	ner.get({
-		port:8080,
-		host:'localhost'
-	}, rawText, (err, res) => {
-		if (err) {
-			console.error(err)
-		} else {
-			console.log(rawText)
-			console.log(res.entities);
-			// Find and replace entities
-			var normalizedText = (' ' + rawText).slice(1);
-			const toReplace = ['PERSON', 'LOCATION', 'ORGANIZATION'];
+var extractEntities = (inputText) => {
+	return new Promise((resolve, reject) => {
+		ner.get({
+			port:8080,
+			host:'localhost'
+		}, inputText, (err, res) => {
+			if (err) {
+				console.error("Could not reach NER service")
+				reject(err);
+			} else {
+				resolve(res.entities);
+			}
+		})
+	})
+};
+
+var normalize = (inputText) => {
+	return extractEntities(inputText)
+		.then(entities => {
+			var normalizedText = (' ' + inputText).slice(1);
+			var toReplace = ['PERSON', 'LOCATION', 'ORGANIZATION'];
 			toReplace.map(namedEntityType => {
-				var foundEntities = res.entities[namedEntityType];
+				var foundEntities = entities[namedEntityType];
 				if (foundEntities) {
 					foundEntities.map(foundEntity => {
 						normalizedText = normalizedText.replace(new RegExp(foundEntity), namedEntityType);
 					})
 				}
 			})
-			console.log(normalizedText)
-		}
+			return normalizedText
+		});
+};
+
+var article = (url) => {
+	return new Promise((resolve, reject) => {
+		axios.get(url)
+			.then(response => {
+				var $ = cheerio.load(response.data)
+				var paragraphs = $('p').map((i, el) => {
+					return $(el).text()
+				})
+				var article = paragraphs.get().join('\n\n')
+				resolve(article)
+			})
+			.catch(error => {
+				reject(error)
+			});
 	});
+};
+
+var normalizeNews = (url) => {
+	return article(url)
+		.then(rawText => {
+			return normalize(rawText)
+		})
 }
 
-normalize(body);
+//normalize("John Smith went to Tesco to buy carrots for dinner.").then(normalizedText => console.log(normalizedText));
+normalizeNews('http://www.bbc.co.uk/news/business-40658774').then(normalizedText => console.log(normalizedText));
+
